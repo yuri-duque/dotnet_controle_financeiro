fc8b6f22cdf00177c32a99cd7def8fd8b2ad04b7
Yuri Duque <yurithielmann83@gmail.com>
2020-08-24 19:57:00 -0300

    criando expenseRepository

---
+++ webapi/Repository/Models/ExpenseRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository.Models
+{
+    class ExpenseRepository : Repository<Income>
+    {
+        public IncomeRepository(BaseContext ctx) : base(ctx) { }
+    }
+}
