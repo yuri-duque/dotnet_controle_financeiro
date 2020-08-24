884683b0c5b7bd476105d5373bcd0069d65641b1
Yuri Duque <yurithielmann83@gmail.com>
2020-08-24 20:38:45 -0300

    criando incomeService

---
+++ webapi/Service/Models/IncomeService.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+
+namespace Service.Models
+{
+    public class IncomeRepository
+    {
+        private readonly IncomeRepository _incomeRepository;
+        private readonly IMapper _mapper;
+
+        public IncomeRepository(IncomeRepository incomeRepository, IMapper mapper)
+        {
+            _incomeRepository = incomeRepository;
+            _mapper = mapper;
+        }
+    }
+}
