39038f279a1e62719a56e7ce16cdbd326e26eb6f
Yuri Duque <yurithielmann83@gmail.com>
2020-08-24 19:56:39 -0300

    criando incomeRepository

---
+++ webapi/Repository/Models/IncomeRepository.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Models;
+using Repository.Context;
+
+namespace Repository.Models
+{
+    public class IncomeRepository : Repository<Income>
+    {
+        public IncomeRepository(BaseContext ctx) : base(ctx) { }
+    }
+}
