62fda60d51503e673dabbfc5af63b84244456318
Yuri Duque <yurithielmann83@gmail.com>
2020-08-24 21:05:32 -0300

    criando mapper expense

---
+++ webapi/Domain/AutoMapper/ExpenseMapping.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Domain.DTO;
+using Domain.Models;
+
+namespace Domain.AutoMapper
+{
+    public class ExpenseMapping : Profile
+    {
+        public ExpenseMapping()
+        {
+            CreateMap<Expense, ExpenseListDTO>().ReverseMap();
+        }
+    }
+}
