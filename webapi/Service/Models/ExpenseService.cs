54a4fe00b1f9a2163c77f45cb8ef26df82a602b5
Yuri Duque <yurithielmann83@gmail.com>
2020-08-26 20:56:57 -0300

    criando Save expenseService

--- webapi/Service/Models/ExpenseService.cs
+++ webapi/Service/Models/ExpenseService.cs
@@ -1,7 +1,9 @@
 ﻿using AutoMapper;
 using Domain.DTO;
+using Domain.Models;
 using Repository.Models;
 using Service.Utils;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -10,21 +12,38 @@ namespace Service.Models
     public class ExpenseService
     {
         private readonly ExpenseRepository _expenseRepository;
+        private readonly WalletRepository _walletRepository;
         private readonly IMapper _mapper;
 
-        public ExpenseService(ExpenseRepository expenseRepository, IMapper mapper)
+        public ExpenseService(ExpenseRepository expenseRepository, WalletRepository walletRepository, IMapper mapper)
         {
             _expenseRepository = expenseRepository;
+            _walletRepository = walletRepository;
             _mapper = mapper;
         }
 
         public ServiceResponse<IList<ExpenseListDTO>> GetAll(long idUser, long idWallet)
         {
-            var incomes = _expenseRepository.GetAllExpenses(idUser, idWallet).ToList();
+            var expenses = _expenseRepository.GetAllExpenses(idUser, idWallet).ToList();
 
-            var incomesDTO = _mapper.Map<IList<ExpenseListDTO>>(incomes);
+            var expensesDTO = _mapper.Map<IList<ExpenseListDTO>>(expenses);
 
-            return ServiceResponse<IList<ExpenseListDTO>>.SetSuccess(incomesDTO);
+            return ServiceResponse<IList<ExpenseListDTO>>.SetSuccess(expensesDTO);
+        }
+
+        public ServiceResponse<Expense> Save(long idUser, ExpenseFormDTO expenseDTO)
+        {
+            var walletExist = _walletRepository.GetById(idUser, expenseDTO.IdWallet);
+
+            if (walletExist == null)
+                return ServiceResponse<Expense>.SetError("Carteira não encontrada!");
+
+            var expense = _mapper.Map<Expense>(expenseDTO);
+            expense.DateRegister = DateTime.Now;
+
+            _expenseRepository.Save(expense);
+
+            return ServiceResponse<Expense>.SetSuccess(expense);
         }
     }
 }
