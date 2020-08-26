6d1b74a717b21cc8b60f52f5534f068d931250c0
Yuri Duque <yurithielmann83@gmail.com>
2020-08-26 20:56:09 -0300

    criando expenseFormDTO

--- webapi/Domain/DTO/ExpenseDTO.cs
+++ webapi/Domain/DTO/ExpenseDTO.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Domain.DTO
 {
@@ -11,5 +12,22 @@ namespace Domain.DTO
         public string Description { get; set; }
 
         public decimal Value { get; set; }
+    }
+
+    public class ExpenseFormDTO
+    {
+        public long Id { get; set; }
+
+        [Required(ErrorMessage = "A data de registro é obrigatória")]
+        public DateTime DateRegister { get; set; }
+
+        [Required(ErrorMessage = "A descrição é obrigatória")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "O valor é obrigatório")]
+        public decimal Value { get; set; }
+
+        [Required(ErrorMessage = "O id da carteira é obrigatória")]
+        public long IdWallet { get; set; }
     }
 }
