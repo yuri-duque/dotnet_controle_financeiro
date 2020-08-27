9f5b3b45a01fbd6f6949e033f1ecef6a87122210
Yuri Duque <yurithielmann83@gmail.com>
2020-08-26 21:03:17 -0300

    criando getById incomeService

--- webapi/Service/Models/IncomeService.cs
+++ webapi/Service/Models/IncomeService.cs
@@ -1,7 +1,9 @@
 ﻿using AutoMapper;
 using Domain.DTO;
+using Domain.Models;
 using Repository.Models;
 using Service.Utils;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -10,11 +12,13 @@ namespace Service.Models
     public class IncomeService
     {
         private readonly IncomeRepository _incomeRepository;
+        private readonly WalletRepository _walletRepository;
         private readonly IMapper _mapper;
 
-        public IncomeService(IncomeRepository incomeRepository, IMapper mapper)
+        public IncomeService(IncomeRepository incomeRepository, WalletRepository walletRepository, IMapper mapper)
         {
             _incomeRepository = incomeRepository;
+            _walletRepository = walletRepository;
             _mapper = mapper;
         }
 
@@ -26,5 +30,16 @@ namespace Service.Models
 
             return ServiceResponse<IList<IncomeListDTO>>.SetSuccess(incomesDTO);
         }
+
+        public ServiceResponse<IList<IncomeFormDTO>> GetById(long idUser, long idIncome)
+        {
+            var income = _incomeRepository.GetById(idUser, idIncome);
+
+            var incomeDTO = _mapper.Map<IList<IncomeFormDTO>>(income);
+
+            return ServiceResponse<IList<IncomeFormDTO>>.SetSuccess(incomeDTO);
+        }
+
+        
     }
 }
